b853e6f0880fb548de91fb2dc127447727c6c3dc
nil-official <niladrichakraborty16@gmail.com>
2024-10-23 18:47:37 +0530

    footer-resp

--- src/LandingPage/components/Footer.jsx
+++ src/LandingPage/components/Footer.jsx
@@ -2,25 +2,23 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='w-full h-[300px] bg-black text-white flex flex-col gap-5 items-center justify-center'>
+        <div className='w-full py-8 lg:py-12 bg-black text-white flex flex-col gap-5 items-center justify-center'>
 
-            <div className='w-[60%] flex flex-col items-center gap-4'>
-                <div className='w-full flex justify-between'>
+            <div className='w-[90%] md:w-[85%] lg:w-[80%] xl:w-[70%] 2xl:w-[60%] flex flex-col items-center gap-4'>
+                <div className='w-full flex flex-col lg:flex-row justify-between gap-6 lg:gap-0'>
                     {/* Left */}
                     <div className='flex flex-col justify-end gap-4'>
-                        <div className='w-[160px] cursor-pointer'>
+                        <div className='w-[130px] xl:w-[130px] 2xl:w-[160px] cursor-pointer'>
                             <img src="images/logo.png" alt="Learnify" />
                         </div>
                         <div>
-                            <ul className='flex gap-4 text-sm'>
+                            <ul className='flex flex-col lg:flex-row text-sm gap-2 xl:gap-2 2xl:gap-4 2xl:text-sm'>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Courses</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Learn</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Workshops</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Topics</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Teachers</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Reviews</li>
-                                <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Guides</li>
-                                <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Blog</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>FAQ</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Join Now</li>
                                 <li className='text-[#636363] hover:text-gray-300 cursor-pointer'>Login</li>
@@ -30,17 +28,17 @@ const Footer = () => {
 
                     {/* Right */}
                     <div className='flex flex-col gap-4'>
-                        <div className='flex gap-4 justify-end'>
-                            <img src="logos/linkedin.svg" alt="linkedin" className='w-7 cursor-pointer' />
-                            <img src="logos/x.svg" alt="x" className='w-7 cursor-pointer' />
-                            <img src="logos/facebook.svg" alt="facebook" className='w-7 cursor-pointer' />
-                            <img src="logos/instagram.svg" alt="instagram" className='w-7 cursor-pointer' />
+                        <div className='flex gap-4 lg:justify-end'>
+                            <img src="logos/linkedin.svg" alt="linkedin" className='w-5 xl:w-5 2xl:w-7 cursor-pointer' />
+                            <img src="logos/x.svg" alt="x" className='w-5 xl:w-5 2xl:w-7 cursor-pointer' />
+                            <img src="logos/facebook.svg" alt="facebook" className='w-5 xl:w-5 2xl:w-7 cursor-pointer' />
+                            <img src="logos/instagram.svg" alt="instagram" className='w-5 xl:w-5 2xl:w-7 cursor-pointer' />
                         </div>
                         <div className='flex gap-4'>
-                            <img src="logos/applestore.svg" alt="applestore" className='w-32 cursor-pointer' />
-                            <img src="logos/googleplay.svg" alt="googleplay" className='w-32 cursor-pointer' />
+                            <img src="logos/applestore.svg" alt="applestore" className='w-28 xl:w-28 2xl:w-32 cursor-pointer' />
+                            <img src="logos/googleplay.svg" alt="googleplay" className='w-28 xl:w-28 2xl:w-32 cursor-pointer' />
                         </div>
-                        <div className='flex justify-end'>
+                        <div className='flex lg:justify-end text-sm'>
                             <p className='text-[#636363] hover:text-gray-300 cursor-pointer'>
                                 Contact: support@learnify.com
                             </p>
@@ -53,7 +51,7 @@ const Footer = () => {
                     <hr className='border-0 h-[2px] bg-[#636363]' />
                 </div>
 
-                <div className='w-full flex text-sm justify-between'>
+                <div className='w-full text-sm lg:flex lg:justify-between'>
                     <div className='flex'>
                         <p className='text-[#636363] hover:text-gray-300 cursor-pointer'>
                             Learnify is proudly made in India, IN
