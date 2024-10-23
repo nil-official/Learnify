e3c61a0fef00e640ab5c0e84f78837a948a2faaa
nil-official <niladrichakraborty16@gmail.com>
2024-10-23 21:35:39 +0530

    section5-updated

--- src/LandingPage/components/Section5.jsx
+++ src/LandingPage/components/Section5.jsx
@@ -3,20 +3,19 @@ import data from '../data/mnc.json'
 
 const Section5 = () => {
     return (
-        <div className='w-full h-[350px] text-white flex flex-col items-center justify-center' style={{ backgroundColor: '#1d1d1d' }}>
-
-            <div className='flex flex-col gap-4 items-center pb-4'>
-                <p className='text-3xl font-bold'>
+        <div className='w-full p-8 lg:py-12 bg-[#1d1d1d] text-white flex flex-col items-center justify-center'>
+            <div className='flex flex-col w-full gap-4 items-center justify-center pb-4'>
+                <p className='text-2xl lg:text-3xl font-bold text-center'>
                     Learn Straight from Experts Who Shape the Modern Web
                 </p>
-                <p className='text-xl font-bold'>
+                <p className='text-lg lg:text-xl font-bold text-center'>
                     Our courses are taught by developers from these top companies (and more)
                 </p>
             </div>
-            <div className='flex items-center justify-center gap-8 py-6'>
+            <div className='flex flex-col items-center justify-center gap-8 py-6 lg:flex-row'>
                 {data.map((mnc, index) => {
                     return (
-                        <div key={index} className='relative w-[130px] cursor-pointer'>
+                        <div key={index} className='relative w-[90px] lg:w-[130px] cursor-pointer'>
                             <img
                                 src={mnc.image}
                                 alt={mnc.alt}
@@ -25,15 +24,13 @@ const Section5 = () => {
                     )
                 })}
             </div>
-            <div className='pt-4'>
+            <div className='pt-4 w-full flex items-center justify-center'>
                 <button
-                    style={{ fontSize: '14px', fontWeight: '650', padding: '6px 18px' }}
-                    className='text-black rounded-full bg-lime-500'
+                    className='text-black text-sm lg:text-[16px] font-bold px-5 py-2 rounded-full bg-lime-500 w-40'
                 >
                     Get Full Access
                 </button>
             </div>
-
         </div>
     )
 }
