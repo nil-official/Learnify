16ec60115449a68a5e3ab9a51d1601ccaad2ac79
nil-official <niladrichakraborty16@gmail.com>
2024-10-24 13:35:01 +0530

    header structural changes

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from './components/Header'
-import Navbar from './components/Navbar'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
 import Section3 from './components/Section3'
@@ -11,25 +10,13 @@ import Footer from './components/Footer'
 const LandingPage = () => {
   return (
     <div>
-      <div className='w-full h-[550px] md:h-[600px] lg:h-[800px] relative'>
-        <img src='images/learning.jpg' className="h-full w-full object-cover" />
-        <div className='absolute inset-0 bg-gradient-to-b from-black via-transparent to-transparent'></div>
-        <div className='absolute inset-0 bg-black opacity-50'></div>
-      </div>
-
-      <div className="absolute top-0 w-full flex flex-col justify-center">
-        <Navbar />
-        <Header />
-      </div>
-
+      <Header />
       <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
       <Section5 />
-
       <Footer />
-
     </div>
   )
 }
--- src/LandingPage/components/Header.jsx
+++ src/LandingPage/components/Header.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Header = () => {
     return (
-        <div className='w-full h-[800px]'>
-
-            <div className='h-full flex flex-col justify-center items-center'>
-                <div className='flex flex-col gap-8 items-center'>
-                    <div className='flex flex-col gap-2 items-center'>
-                        <p style={{ textShadow: '0 0 20px rgba(0, 0, 0, .75)' }} className="text-lg md:text-xl lg:text-2xl font-bold text-white">
-                            Gain Practical Tech Skills from Experts You Can Trust
-                        </p>
-                        <p style={{ textShadow: '0 0 20px rgba(0, 0, 0, .75)' }} className="text-3xl lg:text-5xl font-bold text-white">
-                            Your Path to Senior Developer and Beyond
-                        </p>
-                    </div>
-
-                    <div className='flex gap-8'>
-                        <button className="bg-lime-500 hover:bg-lime-400 text-white font-semibold py-2 px-6 rounded">
-                            Browse Our Courses
-                        </button>
-                        <button className="bg-transparent border-2 border-lime-500 text-lime-500 hover:bg-lime-500 hover:text-white font-semibold py-2 px-6 rounded">
-                            View Learning Paths
-                        </button>
+        <div className='w-full h-[85vh]'>
+            <Navbar />
+            <div className='relative h-full'>
+                <img src='images/learning.jpg' className="h-full w-full object-cover" />
+                <div className='absolute top-0 w-full h-full bg-gradient-to-b from-black via-transparent to-transparent'></div>
+                <div className='absolute top-0 w-full h-full bg-black opacity-50'></div>
+                <div className='absolute top-0 w-full h-full flex flex-col justify-center items-center'>
+                    <div className='flex flex-col gap-8 items-center'>
+                        <div className='flex flex-col gap-2 items-center'>
+                            <p style={{ textShadow: '0 0 20px rgba(0, 0, 0, .75)' }} className="text-lg md:text-xl lg:text-2xl font-bold text-white">
+                                In-Depth Frontend & Fullstack Courses
+                            </p>
+                            <p style={{ textShadow: '0 0 20px rgba(0, 0, 0, .75)' }} className="text-3xl lg:text-5xl font-bold text-white">
+                                Your Path to Senior Developer and Beyond
+                            </p>
+                        </div>
+                        <div className='flex gap-8'>
+                            <button className="bg-lime-500 hover:bg-lime-400 text-white font-semibold py-2 px-6 rounded">
+                                Browse Our Courses
+                            </button>
+                            <button className="bg-transparent border-2 border-lime-500 text-lime-500 hover:bg-lime-500 hover:text-white font-semibold py-2 px-6 rounded">
+                                View Learning Paths
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-            
         </div>
     )
 }
